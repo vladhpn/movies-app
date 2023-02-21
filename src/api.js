e0293d63876598bb88445cb9adeff3fd23099e7b
Vlad Hapon <vladhpn@gmail.com>
2023-02-21 16:43:09 +0000

    add

---
+++ src/api.js
@@ -0,0 +1,3 @@
+export const apiKey = '98e87da0e762537a8cb63c18dd13caee';
+export const imageUrl = 'https://image.tmdb.org/t/p/w500';
+export const posterUrl = 'https://image.tmdb.org/t/p/original';
